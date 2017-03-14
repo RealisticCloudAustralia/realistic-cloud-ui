a73d40a682fe855020e7d1f76c48419a812a617f
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-03-14 13:23:26 +1100

    Added dynamic host and port work in cloud

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ app.get('/',function(req,res){
     res.sendfile('app/index.html',{root:__dirname})
 });
 
-app.listen(8100);
-console.log("App listening on port 8100")
+var server_host = (process.env.HOST || 'localhost'),
+    server_port = (process.env.PORT || 8100);
+    
+app.listen(process.env.PORT || 8100);
+console.log('App listening on port : ' +  server_host + ' ' + server_port);
